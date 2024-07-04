c308e077bde9cc424d7c4a27c5db495ee44c0ef1
Frank Hu <tianyaoh@hotmail.com>
2024-07-04 05:52:04 +0000

    webapp: webcomponent launching.

--- tortoisebot_ros1_docker/webapp/start_services.sh
+++ tortoisebot_ros1_docker/webapp/start_services.sh
@@ -2,7 +2,7 @@
 
 # Source ROS setup
 source /opt/ros/noetic/setup.bash
-source /root/catkin_ws/devel/setup.bash
+source /catkin_ws/devel/setup.bash
 
 # Ensure SLOT_ROSBRIDGE_PORT is set
 if [ -z "$SLOT_ROSBRIDGE_PORT" ]; then
@@ -16,9 +16,6 @@ roslaunch web_components_launching docker_web_components.launch &
 # Give ros web components some time to start
 sleep 5
 
-# Change to the ros directory
-cd /webpage_ws
-
 # Start the Python HTTP server in the foreground
-# cd /path/to/your/webapp  # Make sure to change this to the directory containing your webapp
+cd /webpage_ws/src/tortoisebot_webapp
 python -m SimpleHTTPServer 7000
